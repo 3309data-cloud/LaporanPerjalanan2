53701f241f5c448e332ea2e02ba0a2db77d3549e
3309data-cloud <3309data@gmail.com>
2026-04-21 15:56:10 +0700

    Update Kwitansi.jsx

--- src/components/new/Kwitansi.jsx
+++ src/components/new/Kwitansi.jsx
@@ -26,12 +26,6 @@ export default function Kwitansi({ row }) {
 .print-kwitansi *{
   font-family: "TimesWeb","Times New Roman",serif !important;
 }
-  
-.print-kwitansi table {
-  width: 100% !important;
-  table-layout: fixed !important;
-}
-
 /* teks normal rapat */
 .print-kwitansi p{
   margin:0 !important;
